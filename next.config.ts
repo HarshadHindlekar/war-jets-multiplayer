a0615b0cac9aca119a8bf11e4d64f5077598afca
cloudfinch-harshad <harshad@cloudfinch.com>
2026-03-17 10:41:34 +0530

    comment out basePath and assetPrefix configuration

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export',          // Static HTML export — works on GitHub Pages
   reactStrictMode: false,
   images: { unoptimized: true },
-  // If deployed to a sub-path like /war-jets-multiplayer/ set basePath:
-  basePath: '/war-jets-multiplayer',
-  assetPrefix: '/war-jets-multiplayer/',
+  // If deployed to a sub-path like /war--jets-multiplayer/ set basePath:
+  // basePath: '/war--jets-multiplayer',
+  // assetPrefix: '/war--jets-multiplayer/',
   trailingSlash: true,
 }
 
